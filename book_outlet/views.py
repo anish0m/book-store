d81e2f08c903d4335cba6ec0757dd0b14b713b83
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-02-25 16:52:15 +0600

    Rendering data: Book-Detail

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Book
 
 # Create your views here.
 
+
 def index(request):
-  books = Book.objects.all()
-  return render(request, "book_outlet/index.html", {
-    "books": books
-  })
+    books = Book.objects.all()
+    return render(request, "book_outlet/index.html", {
+        "books": books
+    })
+
+
+def book_detail(request, id):
+    book = get_object_or_404(Book.objects.get(pk=id))
+    return render(request, "book_outlet/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestselling": book.is_bestselling
+    })
