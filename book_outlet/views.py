562e11bb01cefe195b8b7cd8a7a46b6e5ae4f64e
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-02-25 18:10:45 +0600

    Data Aggregation

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404, render
+from django.db.models import Avg
 
 from .models import Book
 
@@ -6,9 +7,15 @@ from .models import Book
 
 
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("-rating")
+
+    no_of_books = books.count()
+    avg_rating = books.aggregate(Avg("rating"))
+
     return render(request, "book_outlet/index.html", {
-        "books": books
+        "books": books,
+        "total_no_of_books": no_of_books,
+        "average_rating": avg_rating
     })
 
 
